90cec925c941f2c1f8264aebf7b1bc4f207aa956
Yongwoo Kim <yongwoo.kim@smu.ac.kr>
2022-05-23 09:17:16 +0900

    update script

--- lab26_block_adder_prj_post/formality/post/formality.tcl
+++ lab26_block_adder_prj_post/formality/post/formality.tcl
@@ -1,3 +1,3 @@
 #set_app_var hdlin_dwroot /opt/SYNOPSYS/DC/syn/P-2019.03
-set_app_var hdlin_dwroot /opt/tools/synopsys/syn/P-2019.03-SP5-6
+set_app_var hdlin_dwroot /tools/synopsys/syn/P-2019.03-SP5-6
 set synopsys_auto_setup true
